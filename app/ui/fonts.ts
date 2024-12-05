765bda948bdee77457b662267647fe3d39b54cf9
ReverTechs <reverblessig@gmail.com>
2024-12-05 17:32:19 +0200

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,33 @@
+// import { Inter, Lusitana } from "next/font/google";
+
+// export const inter = Inter({ subsets: ["latin"] });
+
+// export const lusitana = Lusitana({
+//   weight: ["400", "700"],
+//   subsets: ["latin"],
+// });
+
+import { Inter, Lusitana, Roboto, Arima, Lato } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+// Adding additional Google Fonts
+export const roboto = Roboto({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const arima = Arima({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const lato = Lato({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
